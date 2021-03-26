8a709c7e119569f071dcfa0c53dd2afaca0c36cd
Hajime <63582465+Hajime8673@users.noreply.github.com>
2021-03-26 12:16:54 +0600

    Update __init__.py

--- dpymenus/constants/__init__.py
+++ dpymenus/constants/__init__.py
@@ -1,5 +1,5 @@
-CONFIRM = ['y', 'yes', 'ok', 'k', 'kk', 'ready', 'rdy', 'r', 'confirm', 'okay']
-DENY = ['n', 'no', 'deny', 'negative', 'back', 'return']
-QUIT = ['e', 'exit', 'q', 'quit', 'stop', 'x', 'cancel', 'c']
+CONFIRM = ['y', 'yes']
+DENY = ['n', 'no']
+QUIT = ['cancel', 'c']
 
 GENERIC_BUTTONS = ['⏮️', '◀️', '⏹️', '▶️', '⏭️']
